ce326704f01e49782ba9853fd163caf6c05a906a
rshah_auxita <rshah160487@gmail.com>
2025-01-08 14:53:43 -0700

    Updated Jenkinsfile along with stability of login test

--- e-commerce/src/test/java/com/test/testcases/LoginTest.java
+++ e-commerce/src/test/java/com/test/testcases/LoginTest.java
@@ -3,28 +3,18 @@ package com.test.testcases;
 import com.support.baseTest.Base;
 import com.support.pageobjects.LoginPage;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
-
 @Listeners(com.support.listeners.Listeners.class)
 public class LoginTest extends Base {
     @Test
-    public void testLogin() throws InterruptedException {
+    public void testLogin() {
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.login(properties.getProperty("username"), properties.getProperty("password"));
-        Assert.assertEquals("My Orders", getDriver().findElement(By.linkText("My Orders")).getText());
-        Thread.sleep(2000);
-        String actualText = getDriver().findElement(By.xpath("/html/body/div[2]/header/div[1]/div/ul/li[1]/span")).getText();
-        String expectedText = "Welcome, Neo James!";
-        Assert.assertEquals(actualText, expectedText );
+        Assert.assertEquals(getDriver().getTitle(), "My Account");
+        Assert.assertEquals(getDriver().findElement(By.linkText("My Orders")).getText(), "My Orders");
     }
 
 }
