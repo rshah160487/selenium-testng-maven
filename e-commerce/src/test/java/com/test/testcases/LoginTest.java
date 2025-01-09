1efe1c473054eb18f65b418dda8b844e1d8d976b
rshah_auxita <rshah160487@gmail.com>
2025-01-08 20:58:22 -0700

    Fix - driver issue

--- e-commerce/src/test/java/com/test/testcases/LoginTest.java
+++ e-commerce/src/test/java/com/test/testcases/LoginTest.java
@@ -3,6 +3,7 @@ package com.test.testcases;
 import com.support.baseTest.Base;
 import com.support.pageobjects.LoginPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -11,7 +12,8 @@ import org.testng.annotations.Test;
 public class LoginTest extends Base {
     @Test
     public void testLogin() {
-        LoginPage loginPage = new LoginPage(getDriver());
+        WebDriver driver = Base.getDriver();
+        LoginPage loginPage = new LoginPage(driver);
         loginPage.login(properties.getProperty("username"), properties.getProperty("password"));
         Assert.assertEquals(getDriver().getTitle(), "My Account");
         Assert.assertEquals(getDriver().findElement(By.linkText("My Orders")).getText(), "My Orders");
