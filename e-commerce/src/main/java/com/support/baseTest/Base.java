394baeb3a8bc17c3c5a28605900d2d80c7a333cd
rshah_auxita <rshah160487@gmail.com>
2025-01-09 23:01:06 -0700

    Updated tests for parallel testing

--- e-commerce/src/main/java/com/support/baseTest/Base.java
+++ e-commerce/src/main/java/com/support/baseTest/Base.java
@@ -5,59 +5,65 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-    public class Base {
-        public static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-        public Properties properties;
+public class Base {
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    public Properties properties;
 
-        @BeforeSuite
-        public void setUp() throws IOException {
-            properties = new Properties();
-            FileInputStream fis = new FileInputStream("src/test/resources/utilities/data.properties");
-            properties.load(fis);
+    @BeforeMethod
+    public void setUp() throws IOException {
+        properties = new Properties();
+        FileInputStream fis = new FileInputStream("src/test/resources/utilities/data.properties");
+        properties.load(fis);
 
-            String browser = properties.getProperty("browser");
-            if (browser.equalsIgnoreCase("chrome")) {
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions options = new ChromeOptions();
+        String browser = properties.getProperty("browser");
+        String url = properties.getProperty("url");
+
+        if (driver.get() == null) {
+            initializeDriver(browser);
+        }
+
+        getDriver().manage().window().maximize();
+        getDriver().get(url);
+        System.out.println("Driver initialized and navigated to URL successfully.");
+    }
+
+    private void initializeDriver(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            if (Boolean.parseBoolean(properties.getProperty("headless", "false"))) {
                 options.addArguments("--headless", "--disable-gpu");
-                driver.set(new ChromeDriver(options));
-                System.out.println("ChromeDriver initialized successfully.");
-            } else if (browser.equalsIgnoreCase("edge")) {
-                WebDriverManager.edgedriver().setup();
-                driver.set(new EdgeDriver());
-                System.out.println("EdgeDriver is initialized successfully.");
             }
-
-            if (driver.get() == null) {
-                throw new IllegalStateException("WebDriver initialization failed!");
-            }
-
-            System.out.println("Driver initialized successfully.");
-            getDriver().manage().window().maximize();
-            getDriver().get(properties.getProperty("url"));
+            driver.set(new ChromeDriver(options));
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver.set(new EdgeDriver());
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
+        System.out.println(browser + " WebDriver initialized successfully.");
+    }
 
-        public static WebDriver getDriver()
-        {
-            if (driver.get() == null) {
-                throw new IllegalStateException("Driver is not initialized!");
-            }
-            return driver.get();
+    public static WebDriver getDriver() {
+        if (driver.get() == null) {
+            throw new IllegalStateException("Driver is not initialized for this thread!");
         }
+        return driver.get();
+    }
 
-        @AfterSuite
-        public void tearDown()
-        {
-            if (getDriver() != null) {
-                getDriver().quit();
-                driver.remove();
-            }
+    @AfterMethod
+    public void tearDown() {
+        if (getDriver() != null) {
+            getDriver().quit();
+            driver.remove();
         }
     }
+}
+
 
--- e-commerce/src/test/java/com/test/testcases/LoginTest.java
+++ e-commerce/src/test/java/com/test/testcases/LoginTest.java
@@ -10,13 +10,14 @@ import org.testng.annotations.Test;
 
 @Listeners(com.support.listeners.Listeners.class)
 public class LoginTest extends Base {
+
     @Test
     public void testLogin() {
         WebDriver driver = Base.getDriver();
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(properties.getProperty("username"), properties.getProperty("password"));
-        Assert.assertEquals(Base.getDriver().getTitle(), "My Account");
-        Assert.assertEquals(Base.getDriver().findElement(By.linkText("My Orders")).getText(), "My Orders");
+        Assert.assertEquals(driver.getTitle(), "My Account");
+        Assert.assertEquals(driver.findElement(By.linkText("My Orders")).getText(), "My Orders");
     }
 
 }
